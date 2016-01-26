9387a668009b120ad7a054763fc06848bc2a0db2
Temo Pkhaladze <kverna133@gmail.com>
2016-01-26 19:14:34 +0400

    დავამატე კომენტარები მეტი კოდის წაკითხვადდობისთვის

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
+// პოპაპის გახსნა რეგისტაციაზე კლიკის დროს
 $('#reg').on('click', function(){
  	$('.mainReg').fadeIn("slow");
  	$('body').css('overflow','hidden');
 });
+// პოპაპის გახსნა რეგისტაციაზე კლიკის დროს
+// პოპაპის დახურვა
 $('.closse').on('click', function(){
  	$('.mainReg').fadeOut("slow");
  	$('body').css('overflow','scroll');
 });
+// პოპაპის დახურვა
+// რეგისტრაციდან ავტორიზაციაზე გადასვლა და პირიქით
 $('#aut-form, #reg-form').on('click', function(){
 	$('.forms').toggleClass('hide');
 });
+// რეგისტრაციდან ავტორიზაციაზე გადასვლა და პირიქით
