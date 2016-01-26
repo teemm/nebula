fff138b104ea77af7a98e44938fba1be9cdf3d3b
Temo Pkhaladze <kverna133@gmail.com>
2016-01-26 18:57:10 +0400

    autorization/registration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $('#reg').on('click', function(){
- $('.mainReg').fadeIn("slow");
+ 	$('.mainReg').fadeIn("slow");
 });
 $('.closse').on('click', function(){
- $('.mainReg').fadeOut("slow");
+ 	$('.mainReg').fadeOut("slow");
+});
+$('#aut-form, #reg-form').on('click', function(){
+	$('.forms').toggleClass('hide');
 });
