44cf546fa0fc5922f031a4345bb18daf665f2a08
Temo Pkhaladze <kverna133@gmail.com>
2016-02-03 17:57:36 +0400

    baner animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,30 @@ $('#aut-form, #reg-form').on('click', function(){
 	$('.forms').toggleClass('hide');
 });
 // რეგისტრაციდან ავტორიზაციაზე გადასვლა და პირიქით
+// rows პანელზე ვიდეოს ნაწილზე კლიკის ბეგრაუნდი
 $('#movingSlider li').on('click', function(){
 	$('#movingSlider li').removeClass('active');
 	$(this).addClass('active');
 });
+// rows პანელზე ვიდეოს ნაწილზე კლიკის ბეგრაუნდი სლაიდერი
+$('.goToNext').on('click', function(){
+	animateWidth = '-100%';
+	$('#movingSlider li').removeClass('active');
+	size = parseFloat($('#movingSlider').css('left'));
+	if (size == parseFloat($('#movingSlider').css('width'))/2 * (-1)){
+		animateWidth = 0;
+	};
+	$('#movingSlider').animate({ left: animateWidth });
+
+});
+$('.goToPrev').on('click', function(){
+	animateWidth = '0%';
+	$('#movingSlider li').removeClass('active');
+	if (!parseFloat($('#movingSlider').css('left'))){
+		animateWidth = '-100%';
+	};
+	$('#movingSlider').animate({ left: animateWidth });
+});
+
+
+// სლაიდერი
